5ed43e9b8856b73a67f1b0ff1d02b6b49f9afedd
DanielTakac <danieltakac23@gmail.com>
2022-02-25 18:41:06 +0100

    Added some basic stuff

--- Notes++/Form1.Designer.cs
+++ Notes++/Form1.Designer.cs
@@ -24,21 +24,117 @@
         /// </summary>
         private void InitializeComponent() {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.MainPanel = new System.Windows.Forms.Panel();
+            this.NewButton = new System.Windows.Forms.Button();
+            this.TextArea = new System.Windows.Forms.RichTextBox();
+            this.NewWindowButton = new System.Windows.Forms.Button();
+            this.SaveAsButton = new System.Windows.Forms.Button();
+            this.SaveButton = new System.Windows.Forms.Button();
+            this.OpenButton = new System.Windows.Forms.Button();
+            this.MainPanel.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // MainPanel
+            // 
+            this.MainPanel.Controls.Add(this.SaveAsButton);
+            this.MainPanel.Controls.Add(this.SaveButton);
+            this.MainPanel.Controls.Add(this.OpenButton);
+            this.MainPanel.Controls.Add(this.NewWindowButton);
+            this.MainPanel.Controls.Add(this.NewButton);
+            this.MainPanel.Controls.Add(this.TextArea);
+            this.MainPanel.Location = new System.Drawing.Point(1, 2);
+            this.MainPanel.Name = "MainPanel";
+            this.MainPanel.Size = new System.Drawing.Size(799, 447);
+            this.MainPanel.TabIndex = 0;
+            // 
+            // NewButton
+            // 
+            this.NewButton.Font = new System.Drawing.Font("Yu Gothic UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.NewButton.Location = new System.Drawing.Point(8, 3);
+            this.NewButton.Name = "NewButton";
+            this.NewButton.Size = new System.Drawing.Size(98, 33);
+            this.NewButton.TabIndex = 1;
+            this.NewButton.Text = "New";
+            this.NewButton.UseVisualStyleBackColor = true;
+            this.NewButton.Click += new System.EventHandler(this.NewButton_Click);
+            // 
+            // TextArea
+            // 
+            this.TextArea.BackColor = System.Drawing.SystemColors.Menu;
+            this.TextArea.Location = new System.Drawing.Point(8, 42);
+            this.TextArea.Name = "TextArea";
+            this.TextArea.Size = new System.Drawing.Size(779, 394);
+            this.TextArea.TabIndex = 0;
+            this.TextArea.Text = "";
+            // 
+            // NewWindowButton
+            // 
+            this.NewWindowButton.Font = new System.Drawing.Font("Yu Gothic UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.NewWindowButton.Location = new System.Drawing.Point(112, 3);
+            this.NewWindowButton.Name = "NewWindowButton";
+            this.NewWindowButton.Size = new System.Drawing.Size(98, 33);
+            this.NewWindowButton.TabIndex = 2;
+            this.NewWindowButton.Text = "New Window";
+            this.NewWindowButton.UseVisualStyleBackColor = true;
+            this.NewWindowButton.Click += new System.EventHandler(this.NewWindowButton_Click);
+            // 
+            // SaveAsButton
+            // 
+            this.SaveAsButton.Font = new System.Drawing.Font("Yu Gothic UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.SaveAsButton.Location = new System.Drawing.Point(320, 3);
+            this.SaveAsButton.Name = "SaveAsButton";
+            this.SaveAsButton.Size = new System.Drawing.Size(98, 33);
+            this.SaveAsButton.TabIndex = 2;
+            this.SaveAsButton.Text = "Save As";
+            this.SaveAsButton.UseVisualStyleBackColor = true;
+            this.SaveAsButton.Click += new System.EventHandler(this.SaveAsButton_Click);
+            // 
+            // SaveButton
+            // 
+            this.SaveButton.Font = new System.Drawing.Font("Yu Gothic UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.SaveButton.Location = new System.Drawing.Point(216, 3);
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(98, 33);
+            this.SaveButton.TabIndex = 3;
+            this.SaveButton.Text = "Save";
+            this.SaveButton.UseVisualStyleBackColor = true;
+            this.SaveButton.Click += new System.EventHandler(this.SaveButton_Click);
+            // 
+            // OpenButton
+            // 
+            this.OpenButton.Font = new System.Drawing.Font("Yu Gothic UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.OpenButton.Location = new System.Drawing.Point(424, 3);
+            this.OpenButton.Name = "OpenButton";
+            this.OpenButton.Size = new System.Drawing.Size(98, 33);
+            this.OpenButton.TabIndex = 4;
+            this.OpenButton.Text = "Open";
+            this.OpenButton.UseVisualStyleBackColor = true;
+            this.OpenButton.Click += new System.EventHandler(this.OpenButton_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.MainPanel);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Notes++";
+            this.MainPanel.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private Panel MainPanel;
+        private RichTextBox TextArea;
+        private Button NewButton;
+        private Button SaveAsButton;
+        private Button SaveButton;
+        private Button OpenButton;
+        private Button NewWindowButton;
     }
 }
--- Notes++/Form1.cs
+++ Notes++/Form1.cs
@@ -2,12 +2,44 @@ namespace Notes__ {
 
     public partial class Form1 : Form {
 
+        string path = string.Empty;
+
         public Form1() {
 
             InitializeComponent();
 
         }
 
+        private void NewButton_Click(object sender, EventArgs e) {
+
+
+
+        }
+
+        private void NewWindowButton_Click(object sender, EventArgs e) {
+
+
+
+
+        }
+
+        private void SaveButton_Click(object sender, EventArgs e) {
+
+
+
+        }
+
+        private void SaveAsButton_Click(object sender, EventArgs e) {
+
+
+
+        }
+
+        private void OpenButton_Click(object sender, EventArgs e) {
+
+
+
+        }
     }
 
 }
